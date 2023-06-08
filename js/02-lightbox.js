d3c093b1a22f57a491df1dd183a3b0ef58347ddf
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-06-08 11:44:41 +0300

    final task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryRef = document.querySelector(`.gallery`);
+galleryRef.innerHTML = renderGallery(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', {captionsData: `alt`, captionDelay: 250,});
+
+
+function renderGallery(params) {
+    return params.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`
+    }).join("");
+}
+
 console.log(galleryItems);
